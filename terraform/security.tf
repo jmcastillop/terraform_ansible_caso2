14fe4b0e7f87542f2288a05fa1986849492c4820
jmcastillop <jmcastillopulido@gmail.com>
2022-07-23 18:19:36 +0200

    Eliminación de carpetas

--- terraform/security.tf
+++ terraform/security.tf
@@ -22,7 +22,7 @@ resource "azurerm_network_security_group" "master" {
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "8080"
+        destination_port_range     = ["8080", "80"]
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
